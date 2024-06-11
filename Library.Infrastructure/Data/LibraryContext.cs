600856c2830d7e39b5870d3bee01bd9ad866c59f
Patryk Podsiadło <Patryk.Podsiadlo@microsoft.wsei.edu.pl>
2024-06-11 16:57:42 +0200

    Fic name spaces, adding datacontext

---
+++ Library.Infrastructure/Data/LibraryContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library.Domain.Entities;
+
+
+namespace Infrastructure.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options)
+        {
+        }
+    }
+}
